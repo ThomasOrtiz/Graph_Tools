90b55a6e4e98cb629e95df589c5e515a29e2de9a
grysvn <matt@mattgray.net>
2019-03-18 12:06:32 -0400

    Removes old code and creates new util.
    Starts work on a new utility that adds edge weights given a set of nodes
    and edges.

---
+++ Graph_Tools/src/AddEdgeWeights.java
@@ -0,0 +1,33 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+
+import org.jgrapht.graph.DefaultWeightedEdge;
+
+import Graph.Graph;
+import Graph.Vertex;
+
+/**
+ * Given an input file with many nodes and many edges
+ * and with the line-structure:
+ * 
+ * node-name x-coord y-coord
+ * $
+ * node1 node2
+ * 
+ * Outputs a file with the same nodes and added edge weights
+ * @author grysvn
+ *
+ */
+public class AddEdgeWeights {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+	}
+}
